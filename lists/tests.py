816e43953250f8cfb1655c3d5f65bd80f16340d6
j-000 <jjasilva85@gmail.com>
2020-03-19 21:19:04 +0000

    Basic view return basic html

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,19 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
-
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
     def test_home_page(self):
         url = resolve('/')
         self.assertEqual(url.func, home_page)
+
+    def test_home_page_returns_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def home_page():
-    pass
+def home_page(request):
+    html = """<html>
+                <title>To-do lists</title>
+            </html>"""
+    return HttpResponse(html)
