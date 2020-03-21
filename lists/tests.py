0cf7875be683ac3511ffc6844056a62b2226f881
j-000 <jjasilva85@gmail.com>
2020-03-21 11:41:28 +0000

    Refactored home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
-from django.http import HttpRequest
-from django.template.loader import render_to_string
+
 
 class HomePageTest(TestCase):
 
