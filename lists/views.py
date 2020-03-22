e0dc84dcb463dcb9d8c38672590ebf76e447c7b6
j-000 <jjasilva85@gmail.com>
2020-03-22 14:57:24 +0000

    URLs for forms x2, moved code in views + tests, new url

--- lists/views.py
+++ lists/views.py
@@ -3,10 +3,6 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, template_name='home.html')
 
 
@@ -15,3 +11,8 @@ def view_list(request):
                   template_name='list.html',
                   context={'items': Item.objects.all()}
                   )
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('lists/the-only-list-in-the-world/', views.view_list,
          name='view_list'),
+    path('lists/new', views.new_list, name='new_list'),
     path('admin/', admin.site.urls),
 ]
