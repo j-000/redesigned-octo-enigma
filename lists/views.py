e61b3d4057fa628a1334b61f11fbb1e9e943aa8e
j-000 <jjasilva85@gmail.com>
2020-03-22 14:20:53 +0000

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -6,6 +6,12 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        return redirect('/')
-    return render(request, template_name='home.html',
-                  context={'items':Item.objects.all()})
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, template_name='home.html')
+
+
+def view_list(request):
+    return render(request,
+                  template_name='list.html',
+                  context={'items': Item.objects.all()}
+                  )
